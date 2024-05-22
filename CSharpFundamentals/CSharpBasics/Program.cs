925beca41fd50eb4ff163a280b6850341366a56c
ReshmaEliza <reshma.eli.philip@outlook.com>
2024-05-22 19:58:18 +1200

    CSHARP BASICS

---
+++ CSharpFundamentals/CSharpBasics/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Csharp;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+namespace Csharpfun;
+
+
+
+class Program
+{
+    public void getdata()
+    {
+        Console.WriteLine("Thank You for using our services!");
+    }
+    static void Main(String[] args)
+    {
+        Program p = new Program();
+        p.getdata();
+        Calculator a = new Calculator();
+        a.Adddata(1, 2);
+        a.Division(300, 5);
+        a.Subtract(30, 5);
+        a.Multiply(30, 5);
+
+    }
+}
+
+
